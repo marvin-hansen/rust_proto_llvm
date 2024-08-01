0bde0f68cd611f270bd567fcb7c5d765663b617d
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-01 21:58:30 +0800

    Initial draft of building proto with Rust.

--- samples/rust/hello_lib.rs
+++ samples/rust/hello_lib.rs
@@ -1,11 +1,16 @@
 // Simple Library to generate a proto message
 // Copyright (c) 2024 Radiant Science Inc.
 
-use samples::proto::SampleMessage;
+use proto_bindings::proto::{SampleMessage, SubMessage};
+use std::time::{Duration, SystemTime};
 
 pub fn get_proto() -> SampleMessage {
     SampleMessage {
         name: "Rust".to_string(),
+        time: None,
+        duration: None,
         tags: vec!["rust".to_string(), "proto".to_string()],
+        subs : vec![SubMessage{flag: true, value: 3.4f32}],
+        meta: Some(SubMessage{flag: false, value: 0.0f32}),
     }
 }
--- samples/rust/hello_world.rs
+++ samples/rust/hello_world.rs
@@ -2,7 +2,7 @@
 // Copyright (c) 2024 Radiant Science Inc.
 
 // import hello_lib
-use samples::rust::hello_lib::get_proto;
+use hello_lib::get_proto;
 
 fn main() -> anyhow::Result<()> {
     // create proto msg
